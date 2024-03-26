6abdde908bfdfd2d5567525f695aa1ac5b1937ab
innosoltech <innovativesolgh@gmail.com>
2024-03-26 17:29:37 +0000

    initial commit

---
+++ timer.js
@@ -0,0 +1,36 @@
+//========================================================================================//
+// ............................COUNT DOWN TIMER.............................
+//========================================================================================//
+
+// Set the date we're counting down to
+var countDownDate = new Date("Mar 30, 2024 18:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("days").innerHTML = days + " days ";
+
+  document.getElementById("hours").innerHTML = hours + " hrs ";
+
+  document.getElementById("minutes").innerHTML = minutes + " min ";
+
+  document.getElementById("seconds").innerHTML = seconds + " sec ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
