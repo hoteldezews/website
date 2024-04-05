825f9a2d787bbd9c291c6043c024e29245f21c65
dezews <hoteldezews@gmail.com>
2024-04-05 16:05:51 +0000

    update made

--- timer.js
+++ timer.js
@@ -3,7 +3,7 @@
 //========================================================================================//
 
 // Set the date we're counting down to
-var countDownDate = new Date("Apr 11, 2024 18:00:00").getTime();
+var countDownDate = new Date("Apr 10, 2024 18:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
